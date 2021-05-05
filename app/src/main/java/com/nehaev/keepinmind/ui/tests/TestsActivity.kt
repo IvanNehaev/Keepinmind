d555c5e54a35f8de5d664197895607e8e32deb87
KurtFaust <vanchesable@gmail.com>
2021-05-06 00:20:54 +0300

    Новые функции:  - добавление новых тестов в список через FAB  - реализован поиск по списку тестов

--- app/src/main/java/com/nehaev/keepinmind/ui/tests/TestsActivity.kt
+++ app/src/main/java/com/nehaev/keepinmind/ui/tests/TestsActivity.kt
@@ -1,25 +1,77 @@
 package com.nehaev.keepinmind.ui.tests
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.KeyEvent
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.size
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 import com.nehaev.keepinmind.R
 import com.nehaev.keepinmind.ui.tests.adapter.TestsRecyclerAdapter
+import kotlinx.android.synthetic.main.activity_tests.*
 
 
 class TestsActivity : AppCompatActivity() {
+
+    var testsNames: MutableList<String> = mutableListOf("Kotlin", "Java", "C++", "Swift")
+    var displayList: MutableList<String> = mutableListOf()
+    var nameCounter = 4
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tests)
 
-        val listTests = listOf<String>("Kotlin", "Java", "C++", "Swift")
+        displayList.addAll(testsNames)
 
-        val recyclerView: RecyclerView = findViewById(R.id.rvTests)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = TestsRecyclerAdapter(listTests)
+        fillRecyclerView()
 
+        setFabAction()
 
+        setExploreTestAction()
+    }
+
+    private fun setExploreTestAction() {
+
+        etExploreTests.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {}
+
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+
+                if (etExploreTests.text.isEmpty()) {
+                    displayList.clear()
+                    displayList.addAll(testsNames)
+                } else {
+                    var newDisplayList = testsNames.filter {
+                        it.contains(etExploreTests.text) || it.contains(etExploreTests.text.toString().capitalize())
+                    }
+
+                    displayList.clear()
+                    displayList.addAll(newDisplayList)
+                }
+                rvTests.adapter?.notifyDataSetChanged()
+                rvTests.smoothScrollToPosition(0)
+            }
+        })
+    }
+
+    private fun setFabAction() {
+        fabAddTest.setOnClickListener {
+            nameCounter++
+            Snackbar.make(rvTests, "Name #$nameCounter,  size: ${rvTests.size}", Snackbar.LENGTH_SHORT).show()
+            testsNames.add("#$nameCounter language")
+            displayList.add("#$nameCounter language")
+            rvTests.adapter?.notifyDataSetChanged()
+            rvTests.smoothScrollToPosition(displayList.size)
+        }
+    }
+
+    private fun fillRecyclerView() {
+
+        rvTests.layoutManager = LinearLayoutManager(this)
+        rvTests.adapter = TestsRecyclerAdapter(displayList)
     }
 }
