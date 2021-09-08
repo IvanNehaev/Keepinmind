db3a9631f2263537c0f55d3310d01de94f293c71
Ivan Nehaev <vanchesable@gmail.com>
2021-09-08 23:49:06 +0300

    - Рефактор

--- app/src/main/java/com/nehaev/keepinmind/ui/viewmodels/QuestionCreateViewModel.kt
+++ app/src/main/java/com/nehaev/keepinmind/ui/viewmodels/QuestionCreateViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.navigation.fragment.NavHostFragment
 import com.nehaev.keepinmind.models.Question
 import com.nehaev.keepinmind.models.Theme
 import com.nehaev.keepinmind.repository.MindRepository
@@ -22,6 +23,8 @@ class QuestionCreateViewModel(
         class ValidQuestion : ViewStates()
         class InvalidQuestion : ViewStates()
         class SaveQuestion : ViewStates()
+        class CancelQuestion : ViewStates()
+        class DeleteQuestion : ViewStates()
     }
 
     var questionText: String = ""
@@ -60,24 +63,57 @@ class QuestionCreateViewModel(
     }
 
     fun onButtonSaveClick() {
-
-        theme?.let { theme->
+        theme?.let { theme ->
             // create new question or update existed
             val newQuestion = updateQuestion()
             // write question to DB and update theme question count
             viewModelScope.launch {
-                mindRepository
-                    .questions
-                    .upsertQuestion(newQuestion)
+                mindRepository.questions.upsertQuestion(newQuestion)
                 // update question count if new question has been created
-                if (question == null)
+                if (question == null) {
                     updateThemeQuestionsCount(theme)
-            }
+                    mindRepository.refreshDataInTestsWithTheme(theme)
+                }
 
+            }
             // set save question state on view
             liveData.postValue(ViewStates.SaveQuestion())
         }
+    }
 
+    fun onButtonDeleteClick() {
+        question?.let {
+            // if we edit existed question
+            deleteQuestion(it)
+        }
+        // set cancel/delete/save state on view for exit to questions screen
+        liveData.postValue(ViewStates.CancelQuestion())
+    }
+
+    private fun deleteQuestion(question: Question) = viewModelScope.launch {
+        async {
+            mindRepository.questions.deleteQuestion(question)
+            theme?.let {
+                editTestQuestionCounter(it)
+            }
+        }.await()
+        theme?.let {
+            mindRepository.refreshDataInTestsWithTheme(it)
+        }
+
+    }
+
+    private suspend fun editTestQuestionCounter(theme: Theme) {
+        val dbTheme = mindRepository.themes.getThemeById(theme.id)
+        mindRepository.themes.upsertTheme(
+            Theme(
+                id = dbTheme.id,
+                questionCnt = dbTheme.questionCnt - 1,
+                name = dbTheme.name,
+                categoryId = dbTheme.categoryId,
+                categoryName = dbTheme.categoryName
+            )
+        )
     }
 
     private fun updateQuestion() =
