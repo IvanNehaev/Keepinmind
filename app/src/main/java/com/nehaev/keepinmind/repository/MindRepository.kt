4fec30b38afc33f91cada7328cb4028c8b1a4143
KurtFaust <vanchesable@gmail.com>
2021-09-08 16:38:21 +0300

    - Оптимизация алгоритма обновления числа вопросов в тесте - Изменение числа вопросов в тесте, при создании нового вопроса в теме, включенной в тест

--- app/src/main/java/com/nehaev/keepinmind/repository/MindRepository.kt
+++ app/src/main/java/com/nehaev/keepinmind/repository/MindRepository.kt
@@ -4,10 +4,11 @@ import android.content.Context
 import androidx.lifecycle.LiveData
 import com.nehaev.keepinmind.db.MindDatabase
 import com.nehaev.keepinmind.models.Test
+import com.nehaev.keepinmind.models.Theme
 
 class MindRepository(
-        val db: MindDatabase
-)  {
+    val db: MindDatabase
+) {
 
     val themes = ThemeRepository(db.getThemeDao())
     val categories = CategoryRepository(db.getCategoryDao())
@@ -22,5 +23,57 @@ class MindRepository(
 
     suspend fun upsertTest(test: Test) {
         db.getTestDao().upsert(test)
+    }
+
+    suspend fun refreshDataInTests() {
+        val testsList = tests.getAllTests()
+
+        testsList.forEach { test ->
+            val themesIdList = selectedThemesRepository.getThemesId(test.itemTableName)
+            val selectedThemesList = mutableListOf<Theme>()
+            themesIdList.forEach { themeId ->
+                val theme = themes.getThemeById(themeId)
+                selectedThemesList.add(theme)
+            }
+            val questionCount = selectedThemesList.fold(0) { accumulator, theme ->
+                accumulator + theme.questionCnt
+            }
+            val newTest =
+                Test(
+                    id = test.id,
+                    name = test.name,
+                    questionCnt = questionCount,
+                    rate = test.rate,
+                    itemTableName = test.itemTableName
+                )
+            tests.upsert(newTest)
+        }
+    }
+
+    suspend fun refreshDataInTestsWithTheme(editedTheme: Theme) {
+        val testsList = tests.getAllTests()
+
+        testsList.forEach { test ->
+            val themesIdList = selectedThemesRepository.getThemesId(test.itemTableName)
+            if (themesIdList.contains(editedTheme.id)) {
+                val selectedThemesList = mutableListOf<Theme>()
+                themesIdList.forEach { themeId ->
+                    val theme = themes.getThemeById(themeId)
+                    selectedThemesList.add(theme)
+                }
+                val questionCount = selectedThemesList.fold(0) { accumulator, theme ->
+                    accumulator + theme.questionCnt
+                }
+                val newTest =
+                    Test(
+                        id = test.id,
+                        name = test.name,
+                        questionCnt = questionCount,
+                        rate = test.rate,
+                        itemTableName = test.itemTableName
+                    )
+                tests.upsert(newTest)
+            }
+        }
     }
 }
